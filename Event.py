4eae341158fb5aa7188a5c3a1d6cc5b7a23a5a7f
bruhdrone <sovancej@gmail.com>
2025-12-17 12:38:49 +0530

    Update Event.py
    added placeholder and event id shenanigans

--- Event.py
+++ Event.py
@@ -1,17 +1,40 @@
-import json
 from datetime import date
 
-with open('Events.json', 'r', encoding='utf-8') as file:
-    EventData = json.load(file)
+interhouse,interschool={},{}
 
+EventData = {}
+EventData.update(interhouse)
+EventData.update(interschool)
 
 class event():
 
-    def __init__(event_date:date,event_name:str,grades:set,):
-        if event not in EventData:
-            pass
+#IH ##(CLUB NO.) then 1-12 hexadecimal (6-8, 9-C) GBA
+#IS #### then 1-12 in hexa GBA followed by $'main event name'
+
+    def __init__(self,event_date:date,event_name:str,grades:set,gender ):
+        self.name = event_name
+        self.date = event_date
+        self.grades = grades
+        self,gender = gender
+        event_ID = 'abcd'
+        if event_ID not in EventData:
+            pass#add event
+        else:
+            raise EventError
+
+class placeholder():
+
+    def __init__(self, name, event_set:set[event], ):
+        pass
+    
+class EventError(LookupError):
+
+    def __init__(self, name, message="Event Already Exists:"):
+        super().__init__(f"{message}{name}")
 
 
-class interhouse(event)
+class interhouse(event):
+    pass
 
-class interschool(event)
+class interschool(event):
+    pass
